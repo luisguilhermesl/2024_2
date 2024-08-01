eb408a6b9dd30ed339e89586aebf28ca75e63432
luisguilhermesl <luisgslopes2@gmail.com>
2024-08-01 14:38:32 -0300

    Comentários

--- 2024_2/src/main/java/for_each/numDeVogais.java
+++ 2024_2/src/main/java/for_each/numDeVogais.java
@@ -1,5 +1,5 @@
 package for_each;
-import java.util.Scanner;
+import java.util.Scanner; //Importa a classe Scanner para ler a entrada do usuário.
 
 /**
  *Utilize “for-each" para contar o número de vogais em uma string fornecida pelo usuário.
@@ -8,18 +8,21 @@ import java.util.Scanner;
  */
 public class numDeVogais {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in); //Cria um objeto input da classe Scanner para ler a entrada do usuário.
         
-        System.out.println("Digite uma frase: ");
-        String frase = teclado.nextLine().toLowerCase(); //Converte para minúsculo
-        int contadorVogais = 0;
+        System.out.println("Digite uma frase: "); //Solicita ao usuário que digite uma frase.
+        String frase = teclado.nextLine().toLowerCase(); // Lê a frase digitada, converte para minúsculas e armazena na variável frase. 
         
-        for(char letra : frase.toCharArray()){
-            if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
-                contadorVogais++;
+        int contadorVogais = 0; //Cria uma variável contadorVogais para armazenar a quantidade de vogais e a inicializa com 0.
+        
+        //frase.toCharArray()converte a string frase em um array de caracteres e o laço "for-each" percorre cada caractere (letra) no array.
+        for(char letra : frase.toCharArray()){  
+                                                
+            if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){ //Verifica se o caractere atual (letra) é uma vogal (a, e, i, o, u).
+                contadorVogais++; //Se for uma vogal, incrementa o contadorVogais
             }
         }
-        System.out.println("A frase contém " + contadorVogais + "vogais." );
-        teclado.close();
+        System.out.println("A frase contém " + contadorVogais + " vogais." ); //Imprime a quantidade de vogais encontradas na frase.
+        teclado.close(); //Fecha o objeto Scanner para liberar os recursos.
     }
 }
